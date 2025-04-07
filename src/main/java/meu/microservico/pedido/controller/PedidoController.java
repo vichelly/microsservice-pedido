75be15718fa18691cfa6b03d7e7fb5f33d76835a
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2025-04-07 20:41:12 -0300

    feat: microserviço producer terminado

--- src/main/java/meu/microservico/pedido/controller/PedidoController.java
+++ src/main/java/meu/microservico/pedido/controller/PedidoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -27,10 +28,13 @@ public class PedidoController {
         this.rabbitTemplate = rabbitTemplate;
     }
 
+    @Value("${broker.queue.processamento.name}")
+    private String routingKey;
+
     @PostMapping
     public String criarPedido(@RequestBody Pedido pedido) {
         Pedido pedidoSalvo = pedidoService.salvarPedido(pedido);
-        rabbitTemplate.convertAndSend(pedidoSalvo);
+        rabbitTemplate.convertAndSend("",routingKey,pedidoSalvo.getDescricao());
 
         return "Pedido salvo e enviado para processamento: "+pedido.getDescricao();
     }
