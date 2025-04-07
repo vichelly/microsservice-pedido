fdecc3daead2e00bee20851079847d8afeef169a
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2025-04-07 20:42:15 -0300

    feat: microserviço producer terminado

--- src/main/java/meu/microservico/pedido/repository/PedidoRepository.java
+++ src/main/java/meu/microservico/pedido/repository/PedidoRepository.java
@@ -2,6 +2,8 @@ package meu.microservico.pedido.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import meu.microservico.pedido.model.Pedido;
+
 public interface PedidoRepository extends JpaRepository<Pedido, Long>{
 
 } 
